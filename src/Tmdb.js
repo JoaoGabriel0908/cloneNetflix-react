89aa7fe874730854cd97586c485559d874df6b18
JoaoGabriel0908 <joaogabrielc700@gmail.com>
2022-08-03 22:06:19 -0300

    03/08

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -33,7 +33,7 @@ export default {
             {
                 slug: 'toprated',
                 title: 'Em Alta',
-                items: await basicFetch(`/movie/top_rated/?language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'action',
